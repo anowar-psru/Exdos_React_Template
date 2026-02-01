7c5d6bb5feaac1c61d0863e48e50d2a8e72f5a88
Md. Anuwar Hosan <anowarhpsru@gmail.com>
2026-02-01 22:40:44 +0600

    Team section

--- src/Pages/Home/Home/HomeComponents/TeamMember/TeamMember.jsx
+++ src/Pages/Home/Home/HomeComponents/TeamMember/TeamMember.jsx
@@ -73,44 +73,37 @@ const TeamMember = () => {
         prevArrow: <PrevArrow />,
         nextArrow: <NextArrow />,
         responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
       }
-    ]
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 4,
+      }
+    }
+  ]
     }
     return (
         <>
